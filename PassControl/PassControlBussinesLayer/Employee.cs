36fabccaed8965d5ac12939ffd5ed923220c228f
Weazzzzzzey <heronightmares@gmail.com>
2020-02-07 18:43:29 +0200

    Prideta nauja programa PassControl

---
+++ PassControl/PassControlBussinesLayer/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassControlBussinesLayer
+{
+    public class Employee
+    {
+        public int EmployeeID { get; private set; }
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public List<int> GetIdRights { get; private set; }
+
+    }
+}
---
+++ PassControl/PassControlTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PassControlTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PassControlTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e7bef1f3-3960-47ab-9e84-fa3b63f5326f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
